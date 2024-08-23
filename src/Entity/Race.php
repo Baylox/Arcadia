8495849155cabee437cd80fc281c4256dc62154d
Baylospawn <microsoftbaylo@laposte.net>
2024-08-23 20:27:25 +0200

    Ajout des attributs de Race

--- src/Entity/Race.php
+++ src/Entity/Race.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\RaceRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RaceRepository::class)]
@@ -13,8 +14,53 @@ class Race
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $label = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $habitat_naturel = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(?string $label): static
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getHabitatNaturel(): ?string
+    {
+        return $this->habitat_naturel;
+    }
+
+    public function setHabitatNaturel(?string $habitat_naturel): static
+    {
+        $this->habitat_naturel = $habitat_naturel;
+
+        return $this;
     }
 }

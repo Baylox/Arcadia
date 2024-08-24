49cc917223d2bbb82e180a9c60abac1fcd6d2456
Baylospawn <microsoftbaylo@laposte.net>
2024-08-24 11:36:36 +0200

    Création table Avis + attributs (Sans relations)

---
+++ src/Entity/Avis.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvisRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisRepository::class)]
+class Avis
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $commentaire = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $statut = null;
+
+    #[ORM\Column]
+    private ?bool $is_visible = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getCommentaire(): ?string
+    {
+        return $this->commentaire;
+    }
+
+    public function setCommentaire(string $commentaire): static
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function isVisible(): ?bool
+    {
+        return $this->is_visible;
+    }
+
+    public function setVisible(bool $is_visible): static
+    {
+        $this->is_visible = $is_visible;
+
+        return $this;
+    }
+}
